990b16cf970cd6ff191ac342a460144a9247563d
Nguyen Ngoc Duc <55783340+nnduc2207@users.noreply.github.com>
2021-10-26 16:12:36 +0700

    Merge Login route and AuthRequire to App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Redirect, Route, Switch } from "react-router"
+import Login from "./components/Login/Login"
+import Home from "./components/Home/Home"
+import { useDispatch, useSelector } from "react-redux"
+import DecisionRoute from "./components/hoc/DecisionRoute"
+import { useEffect, useState } from "react"
+import { getUser } from "./helper/auth"
+import { LOGOUT } from "./store/actions"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const dispatch = useDispatch()
+    const token = useSelector((state) => state.token)
+    const [user, setUser] = useState(null)
+    const [errorMsg, setErrorMsg] = useState(null)
+
+    useEffect(() => {
+        const getUserInfo = async () => {
+            if (!token) {
+                setErrorMsg("Require login")
+            }
+
+            try {
+                const result = await getUser(token)
+                setUser(result)
+            } catch (error) {
+                dispatch({ type: LOGOUT })
+                setErrorMsg(error)
+            }
+        }
+        getUserInfo()
+    }, [token])
+
+    return (
+        <div className="App">
+            <Switch>
+                <DecisionRoute
+                    path="/login"
+                    trueComponent={Login}
+                    falseComponent={Home}
+                    decision={user == null}
+                />
+                <DecisionRoute
+                    path="/"
+                    trueComponent={Home}
+                    falseComponent={Login}
+                    decision={user != null}
+                />
+            </Switch>
+        </div>
+    )
 }
 
-export default App;
+export default App
